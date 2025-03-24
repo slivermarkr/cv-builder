be2ef92919ef5f88ffe82ab56f3a9a14f1363e44
sliver <125364267+slivermarkr@users.noreply.github.com>
2025-03-24 12:34:44 +0800

    preserving state on next button

--- src/startover/App.jsx
+++ src/startover/App.jsx
@@ -1,36 +1,112 @@
 import { useState } from "react";
+/**
+ *  const formArray = [
+ * {title: '',id: 0,childrenInput: [{type, id, label, value}],},
+ * ]
+ */
+const formsInit = [
+  {
+    title: "Contact",
+    childrenInput: [
+      { type: "text", id: "try", label: "Put something in here: ", value: "" },
+    ],
+  },
+  {
+    title: "Work Experience",
+    childrenInput: [
+      { type: "text", id: "try", label: "Put something in here: ", value: "" },
+    ],
+  },
+  {
+    title: "Career Summary",
+    childrenInput: [
+      { type: "text", id: "try", label: "Put something in here: ", value: "" },
+    ],
+  },
+  {
+    title: "Education",
+    childrenInput: [
+      { type: "text", id: "try", label: "Put something in here: ", value: "" },
+    ],
+  },
+];
 
-const formNames = ["Contact", "Work Experience", "Career Summary"];
 export default function App() {
-  const [currForm, setCurrForm] = useState(0);
+  const [currFormIndex, setCurrForm] = useState(0);
+  const [forms, setForms] = useState(formsInit);
 
-  console.log(currForm);
+  console.log(currFormIndex);
   function handleNextClick() {
     setCurrForm((curr) => curr + 1);
-    if (currForm >= formNames.length - 1) {
+    if (currFormIndex >= forms.length - 1) {
       setCurrForm(0);
     }
   }
+  function handleBackClick() {
+    setCurrForm((curr) => curr - 1);
+    if (currFormIndex <= 0) {
+      setCurrForm(forms.length - 1);
+    }
+  }
+
+  function handleInputChange(e) {
+    const targetForm = forms[currFormIndex];
+    const text = e.target.value;
+
+    setForms(
+      forms.map((form) => {
+        if (form.id === targetForm.id) {
+          targetForm.childrenInput.map((child) => {
+            if (child.id === e.target.id) {
+              child.value = text;
+            }
+            return child;
+          });
+        }
+        return form;
+      })
+    );
+  }
+
   return (
     <div className="container">
+      <button onClick={handleBackClick}>Back</button>
       <button onClick={handleNextClick}>Next</button>
-      {formNames.map((names, id) => {
-        if (id === currForm) {
-          return <Form name={names} key={id} />;
+      {forms.map((form, id) => {
+        if (id === currFormIndex) {
+          return (
+            <Form
+              name={form.title}
+              key={id}
+              onChange={handleInputChange}
+              childArray={form.childrenInput}
+            />
+          );
         }
       })}
     </div>
   );
 }
-
-function Form({ name, id }) {
+function Form({ name, id, onChange, childArray }) {
   return (
     <form id={id}>
       <h3>{name}</h3>
-      <p>
-        <label htmlFor="inp">Put some input here: </label>
-        <input type="text" name="" id="inp" />
-      </p>
+      {childArray.map((child, index) => {
+        return (
+          <p key={index}>
+            <label htmlFor={child.id}>Put some input here: </label>
+            <input
+              datatype={index}
+              onChange={onChange}
+              type={child.type}
+              value={child.value}
+              name=""
+              id={child.id}
+            />
+          </p>
+        );
+      })}
+      <p></p>
     </form>
   );
 }
